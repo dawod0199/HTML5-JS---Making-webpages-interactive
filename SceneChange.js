8d3743452299920660a3e8a8f0913af763462deb
dawod0199 <39832780+dawod0199@users.noreply.github.com>
2019-01-27 22:47:00 -0500

    Create SceneChange.js

---
+++ SceneChange.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Project: Scene change</title>
+        <style>
+        #leafers {
+            position: absolute;
+            top: 210px;
+            left: 378px;
+        }
+            
+        </style>
+    </head>
+    <body>
+
+    <img src="https://www.kasandbox.org/programming-images/landscapes/beach-with-palm-trees.png">
+
+    <img src="https://www.kasandbox.org/programming-images/avatars/leafers-seedling.png" id="leafers">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script>
+        $("#leafers").on("click", function(event) {
+            // Do something!
+        });
+    </script>
+    </body>
+</html>
